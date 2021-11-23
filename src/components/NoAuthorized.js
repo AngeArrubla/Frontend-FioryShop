d7184b98c7b8088f079d681614d34c76668d5299
Angelica Arrubla <ange.arrubla@gmail.com>
2021-11-23 12:07:07 -0500

    estilos en el componente NoAuthorized

--- src/components/NoAuthorized.js
+++ src/components/NoAuthorized.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import '../styles/NoAuthorized.scss';
 
 export default function NoAuthorized() {
     return (
-        <div>
-            <img src='https://thumbs.dreamstime.com/b/restricted-area-do-not-enter-authorized-personnel-symbol-sign-vector-illustration-isolate-white-background-label-eps-204139968.jpg' />
-            <img src= 'https://i.imgur.com/WRxL3BI.jpeg' />
-            <h1>Inicia sesión con tu usuario y contraseña</h1>
+        <div id="noAuthorized">
+            <img src= 'https://i.imgur.com/As2uvYT.jpg' alt="Stop" />
+            <h1>¡Inicia sesión con tu usuario y contraseña!</h1>
         </div>
     )
 }
